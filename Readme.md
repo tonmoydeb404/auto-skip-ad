0ac63879c381fb088a2cae66da9eabb5ba33ea52
tonmoydeb404 <tonmoydeb404@gmail.com>
2023-10-17 19:17:31 +0600

    first commit

---
+++ Readme.md
@@ -0,0 +1,36 @@
+# Auto Skip Ad - YouTube
+
+**Introduction:**
+Tired of intrusive ads disrupting your online experience, but want to maintain ethical online practices? We have a solution for you! Our script doesn't block ads; it simply mutes and skips them after a set time, allowing you to enjoy content interruption-free while still supporting content creators.
+
+## Why This Script?
+
+By using this script, you can make your online experience more enjoyable without completely blocking ads.
+
+- Avoid the annoyance of disruptive ads.
+- Maintain ethical online behavior by not blocking ads entirely.
+- Strike a balance between ad-supported content and a smoother viewing experience.
+- **Automatic Ad Skipping:** Our script will automatically skip ads after just 5 seconds, ensuring you get to the content you love without the wait.
+
+## How to Use This Script:
+
+1. **Install TamperMonkey Extension**:
+   First, ensure you have the TamperMonkey extension installed in your browser. If you don't have it yet, you can download it from your browser's extension store.
+
+   - [Tampermonkey for Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+   - [Tampermonkey for Firefox](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/)
+   - [Tampermonkey for Edge](https://microsoftedge.microsoft.com/addons/detail/tampermonkey/iikmkjmpaadaobahmlepeloendndfphd)
+   - [Tampermonkey for Opera/OperaGX](https://addons.opera.com/en-gb/extensions/details/tampermonkey-beta/)
+
+2. **Install Our Script**:
+   Click [here](https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js) to install our script.
+
+## Contributor:
+
+- [Tonmoy Deb](https://tonmoydeb.com)
+
+## Disclaimer:
+
+While this script allows you to skip and mute ads after just 5 seconds, it's essential to remember that content creators rely on ad revenue to support their work. Blocking ads completely may harm the ecosystem that enables free content on the internet. This script offers a compromise to strike the right balance.
+
+Enjoy a more seamless viewing experience while respecting the ethics of online content consumption.
---
+++ index.js
@@ -0,0 +1,106 @@
+// ==UserScript==
+// @name         Auto Skip Ad
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  automatic skip youtube ads!
+// @author       tonmoydeb404
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+/*
+#	Title : Auto Skip Ad
+#	Description : automatic skip youtube ads!
+#	Author : Tonmoy Deb (tonmoydeb404@gmail.com)
+#	Date : 17/10/2023
+*/
+
+(function () {
+  "use strict";
+
+  // constant values.
+  const AD_CLASS = "ad-showing";
+  const SKIP_TIME = 5000;
+
+  /**
+   * get the video element
+   * @returns {Element}
+   */
+  const getVideoElem = () => document.querySelector("video.html5-main-video");
+
+  /**
+   * Automatic AD handler: Mute AD, Skip AD
+   * @param {Element} media
+   */
+  const adHandler = (media) => {
+    // abort function if media is not a valid element
+    if (!media) return;
+
+    // movie player element
+    const moviePlayer = document.querySelector("#movie_player");
+
+    // check Ad is currently playing or not
+    if (moviePlayer.classList.contains(AD_CLASS)) {
+      // mute the ad
+      media.muted = true;
+
+      // waiting to click skip button
+      setTimeout(() => {
+        // make sure skip button is available & not disabled
+        const skipBtn = document.querySelector("button.ytp-ad-skip-button");
+        if (!skipBtn) return;
+
+        // skip the ad
+        skipBtn.click();
+      }, SKIP_TIME);
+    } else {
+      // unmute video
+      media.muted = false;
+    }
+  };
+
+  /**
+   * check url contains "youtube.com/watch" string or not
+   * @param {string} url
+   * @returns {Boolean}
+   */
+  const urlChecker = (url) => url.includes("youtube.com/watch");
+
+  /**
+   * Detect page url change and update video event listener
+   */
+  const observeUrlChange = () => {
+    // variable for previous url. initially it will be undefined
+    let prev_url = undefined;
+
+    const observer = new MutationObserver((_mutations) => {
+      const current_url = document.location.href;
+
+      // check previous url and check url changed or not
+      if (prev_url && prev_url === current_url) return;
+
+      // if url changed then update the previous url
+      prev_url = current_url;
+
+      if (!urlChecker(current_url)) return;
+
+      // add event listener to the video
+      const mainVideo = getVideoElem();
+      if (!mainVideo) return;
+      mainVideo.oncanplay = (e) => adHandler(e.target);
+    });
+
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  };
+
+  // assign observer on window load
+  window.onload = () => {
+    if (urlChecker(window.location.href)) adHandler(getVideoElem());
+
+    observeUrlChange();
+  };
+})();
